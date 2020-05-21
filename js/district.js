9f7d0c4860c2d4e77bf3da90f4b18cb0423c8fe9
PriyanshuDangi <priyanshudangipd@gmail.com>
2020-05-21 22:05:58 +0530

    added state-wise cases and dark mode

--- js/district.js
+++ js/district.js
@@ -1,8 +1,4 @@
-// const messageOne = document.getElementById('message-1')
-// const messageTwo = document.getElementById('message-2')
-// const messageThree = document.getElementById('message-3')
-// const messageFour = document.getElementById('message-4')
-
+//message
 const messageOne = document.getElementById('message-1')
 const messageTwo = document.getElementById('message-2')
 const messageThree = document.getElementById('message-3')
@@ -17,41 +13,35 @@ const dateSpan = document.getElementById('dateSpan')
 const dateDiv = document.getElementById('datediv')
 const statesInput = document.getElementById('statesDropdown')
 const loading = document.getElementById('loading')
-
-
 const statesDropdown = document.querySelector('#states')
 const districtsDropdown = document.querySelector('#districtsDropdown')
-let wholeData;
 
-fetch('https://api.covid19india.org/state_district_wise.json').then((response)=>{
-    response.json().then((data)=>{
-        wholeData = data;
-        let states = Object.keys(data)
-        // console.log(states)
-        let stateHTML = '<option> Please Select your state</option>'
-        let p;
-        for(p = 1; p < states.length; p++){
-            stateHTML += `<option value = "${states[p]}">${states[p]}</option>`
-        }
-        statesDropdown.innerHTML = stateHTML
-        loading.textContent = ''
-        statesInput.style.display = "block";
+var wholeData;
+function district(){    
+    fetch('https://api.covid19india.org/state_district_wise.json').then((response)=>{
+        response.json().then((data)=>{
+            wholeData = data;
+            let states = Object.keys(data)
+            let stateHTML = '<option> Please Select your state</option>'
+            let p;
+            for(p = 1; p < states.length; p++){
+                stateHTML += `<option value = "${states[p]}">${states[p]}</option>`
+            }
+            statesDropdown.innerHTML = stateHTML
+            loading.textContent = ''
+            statesInput.style.display = "block";
+        })
+    }).catch((error)=>{
+        loading.textContent = 'failed to load. Please check your connection!'
     })
-}).catch((error)=>{
-    loading.textContent = 'failed to load. Please check your connection!'
-    // console.log('failed to load. Please check your connection!')
-})
 
+    statesDropdown.addEventListener('change', districtState)
+}
+district();
 
-let districts;
-let stateHTML;
-let districtsData;
-let districtName;
-statesDropdown.addEventListener('change', ()=>{
-
+function districtState(){
     section.style.display = "none";
     if(!wholeData[statesDropdown.value]){
-        // return document.getElementById('error').textContent = "please select a valid option"
         return districtsDropdown.textContent = "please select a valid option"
     }
 
@@ -69,52 +59,91 @@ statesDropdown.addEventListener('change', ()=>{
     document.getElementById('districts').addEventListener('change', ()=>{
         districtName = document.getElementById('districts').value
         var data = districtsData[districtName];
-        console.log(data)
-
-            // messageOne.textContent = data.confirmed;
-            // messageTwo.textContent = data.active;
-            // messageThree.textContent = data.recovered;
-            // messageFour.textContent = data.deceased;
-            messageOne.textContent = data.confirmed;
-            messageTwo.textContent = data.deceased;
-            messageThree.textContent = data.recovered;
-            messageFour.textContent = data.delta.confirmed;
-            messageFive.textContent = data.delta.deceased;
-            messageSix.textContent = data.delta.recovered;
-            section.style.display = "block";
+        messageOne.textContent = data.confirmed;
+        messageTwo.textContent = data.deceased;
+        messageThree.textContent = data.recovered;
+        messageFour.textContent = data.delta.confirmed;
+        messageFive.textContent = data.delta.deceased;
+        messageSix.textContent = data.delta.recovered;
+        section.style.display = "block";
     })
-})
+}
 
+let districts;
+let stateHTML;
+let districtsData;
+let districtName;
 
 function india(){
     loading.textContent = 'loading...'
-    // statesInput.style.display = "none";
     districtInput.style.display = "none"
     section.style.display = "none"
     fetch('https://api.covid19india.org/data.json').then((response)=>{
     response.json().then((indiadata)=>{
-        // console.log(data.cases_time_series.splice(-1)[0])
-        // var data = data.cases_time_series.splice(-1)[0];
-        // console.log(data)
-        // var hey = indiadata.cases_time_series
-        // console.log(indiadata.cases_time_series)
-        var data = indiadata.cases_time_series[indiadata.cases_time_series.length - 1]
-        // var fulldata = indiadata.cases_time_series[111]
+        var data = indiadata.statewise[0]
         console.log(data)
-        // console.log(fulldata)
-            // error.textContent = ""
-
-            loading.textContent = 'Last Updated:' + data.date
-            // loading.textContent = ''
-            // document.getElementById('dateSpan').textContent = data.Date
-            messageOne.textContent = data.totalconfirmed;
-            messageTwo.textContent = data.totaldeceased;
-            messageThree.textContent = data.totalrecovered;
-            messageFour.textContent = data.dailyconfirmed;
-            messageFive.textContent = data.dailydeceased;
-            messageSix.textContent = data.dailyrecovered;
+            loading.textContent = 'Last Updated:' + data.lastupdatedtime
+            messageOne.textContent = data.confirmed;
+            messageTwo.textContent = data.deaths;
+            messageThree.textContent = data.recovered;
+            messageFour.textContent = data.deltaconfirmed;
+            messageFive.textContent = data.deltadeceased;
+            messageSix.textContent = data.deltarecovered;
             section.style.display = "block";
-            // dateDiv.style.display = "block";
     })
     })
+}
+
+let stateData;
+function state(){
+    statesDropdown.removeEventListener('change', districtState)
+    districtInput.style.display = "block"
+    section.style.display = "none";
+    districtsDropdown.textContent = ''
+    fetch('https://api.covid19india.org/data.json').then((response)=>{
+    response.json().then((data)=>{
+        stateData = data.statewise;
+        let states = stateData
+        let stateHTML = '<option value="notallowed"> Please Select your state</option>'
+        let p;
+        for(p = 1; p < states.length; p++){
+            var statename = states[p].state
+            stateHTML += `<option value = "${p}">${statename}</option>`
+        }
+        statesDropdown.innerHTML = stateHTML
+        loading.textContent = ''
+        statesInput.style.display = "block";
+    })
+}).catch((error)=>{
+    loading.textContent = 'failed to load. Please check your connection!'
+    // console.log('failed to load. Please check your connection!')
+})
+
+statesDropdown.addEventListener('change', statestate)
+}
+
+function statestate(){
+
+        // section.style.display = "none";
+        if(statesDropdown.value === "notallowed"){
+            loading.textContent = ''
+            return districtsDropdown.textContent = "please select a valid option"
+        }
+            var index = parseInt(statesDropdown.value)
+            var data = stateData[index]
+            for(i=1; i < data.length; i++){
+                if(data[i].state == statesDropdown.value){
+                    data = data[i]
+                }
+            }
+            console.log(data)
+            districtsDropdown.textContent = ""
+                loading.textContent = 'Last Updated:' + data.lastupdatedtime
+                messageOne.textContent = data.confirmed;
+                messageTwo.textContent = data.deaths;
+                messageThree.textContent = data.recovered;
+                messageFour.textContent = data.deltaconfirmed;
+                messageFive.textContent = data.deltadeaths;
+                messageSix.textContent = data.deltarecovered
+                section.style.display = "block";
 }
